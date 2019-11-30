114334b4919e1156f68671905de9ac223db90594
andryxins <andryxins@icloud.com>
2019-11-30 11:10:02 +0200

    fixed bugs

--- Module-3/task-5.js
+++ Module-3/task-5.js
@@ -10,9 +10,7 @@ const products = [
 const getAllPropValues = (arr, prop) => {
   const propValues = [];
   for (const item of arr) {
-    if (item[prop]) {
-      propValues.push(item[prop]);
-    }
+    if (item[prop]) propValues.push(item[prop]);
   }
   return propValues;
 };
