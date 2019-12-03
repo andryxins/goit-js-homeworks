7b8a2349832f617a643f00b5096b49d8e2ea614f
andryxins <andryxins@icloud.com>
2019-12-03 11:21:29 +0200

    fixed comments in task-3

--- Module-5/task-3.js
+++ Module-5/task-3.js
@@ -14,12 +14,6 @@ class Storage {
   }
 
   removeItem(item) {
-    // for (let i = 0; i < this.items.length; i += 1) {
-    //   if (this.items[i] === item) {
-    //     this.items.splice(i, 1);
-    //     break;
-    //   }
-    // }
     const isItemInStorage = this.items.indexOf(item);
     if (isItemInStorage >= 0) this.items.splice(isItemInStorage, 1);
   }
@@ -31,7 +25,6 @@ const storage = new Storage([
   'Железные жупи',
   'Антигравитатор',
 ]);
-console.log(storage);
 
 const items = storage.getItems();
 console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
