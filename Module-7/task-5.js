c2be742bac745e3a3baf233436dda66a0ec3013c
andryxins <andryxins@icloud.com>
2019-12-06 16:59:41 +0200

    added module-7 task-5

---
+++ Module-7/task-5.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', handleInputName);
+
+function handleInputName() {
+  input.value.length > 0
+    ? (output.textContent = input.value)
+    : (output.textContent = 'незнакомец');
+}
