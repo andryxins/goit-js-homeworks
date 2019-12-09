ac88a98e0ea478a9376038780ffa1c2dc212d625
andryxins <andryxins@icloud.com>
2019-12-09 19:44:32 +0200

    fixed task-4

--- Module-7/task-4.js
+++ Module-7/task-4.js
@@ -1,20 +1,44 @@
 'use strict';
 
-const counter = document.querySelector('#value');
-let counterValue = 0;
-counter.textContent = counterValue;
-const increment = document.querySelector('button[data-action="increment"]');
-const decrement = document.querySelector('button[data-action="decrement"]');
+// const counter = document.querySelector('#value');
+// let counterValue = 0;
+// counter.textContent = counterValue;
+// const increment = document.querySelector('button[data-action="increment"]');
+// const decrement = document.querySelector('button[data-action="decrement"]');
 
-increment.addEventListener('click', handleCounterIncrement);
-decrement.addEventListener('click', handleCounterDecrement);
+// increment.addEventListener('click', handleCounterIncrement);
+// decrement.addEventListener('click', handleCounterDecrement);
 
-function handleCounterIncrement() {
-  counterValue += 1;
-  counter.textContent = counterValue;
-}
+// function handleCounterIncrement() {
+//   counterValue += 1;
+//   counter.textContent = counterValue;
+// }
 
-function handleCounterDecrement() {
-  counterValue -= 1;
-  counter.textContent = counterValue;
-}
+// function handleCounterDecrement() {
+//   counterValue -= 1;
+//   counter.textContent = counterValue;
+// }
+
+const refs = {
+  value: document.querySelector('#value'),
+  counter: document.querySelector('#counter'),
+};
+
+const actions = {
+  state: {
+    value: 0,
+  },
+  decrement() {
+    this.state.value -= 1;
+  },
+  increment() {
+    this.state.value += 1;
+  },
+};
+
+const changeValue = ({ target }) => {
+  actions[target.dataset.action]();
+  refs.value.textContent = actions.state.value;
+};
+
+refs.counter.addEventListener('click', changeValue);
