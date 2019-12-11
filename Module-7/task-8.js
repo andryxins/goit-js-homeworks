acd1c15458f110c494b1040f199facfb8f988dbb
andryxins <andryxins@icloud.com>
2019-12-11 11:52:31 +0200

    fixed task-8 and added part of module-8

--- Module-7/task-8.js
+++ Module-7/task-8.js
@@ -7,24 +7,40 @@ const boxes = document.querySelector('#boxes');
 submitBtn.addEventListener('click', () => createBoxes(input.value));
 clearBtn.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
-  let numberOfBox = 0;
-  let size = 30;
-  let resaltBoxes = '';
-  while (numberOfBox < Number(amount)) {
-    const backgroundColor = `rgb(${Math.floor(
-      Math.random() * 255,
-    )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
-      Math.random() * 255,
-    )})`;
-    const newBox = `<div style="background-color: ${backgroundColor}; width: ${size}px; height: ${size}px;"></div>`;
-    resaltBoxes += newBox;
-    size += 10;
-    numberOfBox += 1;
-  }
-  boxes.insertAdjacentHTML('beforeend', resaltBoxes);
+function createBoxes(amount, size = 30) {
+  const resaltBoxes = Array(Number(amount))
+    .fill('')
+    .map((item, idx) => {
+      const backgroundColor = `rgb(${Math.floor(
+        Math.random() * 255,
+      )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
+        Math.random() * 255,
+      )})`;
+      const newBox = `<div style="background-color: ${backgroundColor}; width: ${size +
+        idx * 10}px; height: ${size + idx * 10}px;"></div>`;
+      return (item += newBox);
+    });
+  boxes.insertAdjacentHTML('beforeend', resaltBoxes.join(''));
 }
 
 function destroyBoxes() {
   boxes.innerHTML = '';
 }
+
+// function createBoxes(amount) {
+//   let numberOfBox = 0;
+//   let size = 30;
+//   let resaltBoxes = '';
+//   while (numberOfBox < Number(amount)) {
+//     const backgroundColor = `rgb(${Math.floor(
+//       Math.random() * 255,
+//     )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
+//       Math.random() * 255,
+//     )})`;
+//     const newBox = `<div style="background-color: ${backgroundColor}; width: ${size}px; height: ${size}px;"></div>`;
+//     resaltBoxes += newBox;
+//     size += 10;
+//     numberOfBox += 1;
+//   }
+//   boxes.insertAdjacentHTML('beforeend', resaltBoxes);
+// }
---
+++ Module-8 gallery/js/gallery-render.js
@@ -0,0 +1,23 @@
+'use strict';
+import images from '../gallery-items.js';
+
+const galleryList = document.querySelector('.js-gallery');
+const markUp = images.map(image => renderGalleryItems(image)).join('');
+galleryList.insertAdjacentHTML('beforeend', markUp);
+const options = {};
+const observer = new IntersectionObserver(onEntry, options);
+const galleryItems = document.querySelectorAll('.gallery__item');
+galleryItems.forEach(item => observer.observe(item));
+
+function renderGalleryItems({ preview, original, description }) {
+  return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="" data-preview="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
+}
+function onEntry(entries, self) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const lazyImg = entry.target.querySelector('.gallery__image');
+      lazyImg.src = lazyImg.dataset.preview;
+      self.unobserve(entry.target);
+    }
+  });
+}
