5f5c2822f8a14052fb954ef8a76ee2eccef2c3ba
andryxins <andryxins@icloud.com>
2019-12-30 11:08:47 +0200

    added Module-11-timer

---
+++ Module-11-timer/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    (this.selector = document.querySelector(selector)),
+      (this.targetDate = targetDate.getTime()),
+      (this.daysLeft = this.selector.querySelector('span[data-value="days"]')),
+      (this.hoursLeft = this.selector.querySelector(
+        'span[data-value="hours"]',
+      )),
+      (this.minutesLeft = this.selector.querySelector(
+        'span[data-value="mins"]',
+      )),
+      (this.secondsLeft = this.selector.querySelector(
+        'span[data-value="secs"]',
+      ));
+  }
+
+  formattingDate(item) {
+    return String(item).padStart(2, '0');
+  }
+
+  updateClockFace() {
+    const timeLeft = this.targetDate - Date.now();
+    this.daysLeft.textContent = this.formattingDate(
+      Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
+    );
+    this.hoursLeft.textContent = this.formattingDate(
+      Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    this.minutesLeft.textContent = this.formattingDate(
+      Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+    );
+    this.secondsLeft.textContent = this.formattingDate(
+      Math.floor((timeLeft % (1000 * 60)) / 1000),
+    );
+  }
+  start() {
+    setInterval(() => {
+      this.updateClockFace();
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2020'),
+});
+console.log(timer);
+
+timer.start();
+
+const timer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Dec 31, 2019'),
+});
+
+timer2.start();
