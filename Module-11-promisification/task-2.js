c73bf213025b158217a570e4ccb29bc5ad913558
andryxins <andryxins@icloud.com>
2020-01-27 10:09:01 +0200

    fixed promise

--- Module-11-promisification/task-2.js
+++ Module-11-promisification/task-2.js
@@ -8,15 +8,25 @@ const users = [
 ];
 
 const toggleUserState = (allUsers, userName) => {
-  return new Promise(resolve => {
-    const updatedUsers = allUsers.map(user =>
+  return Promise.resolve(
+    allUsers.map(user =>
       user.name === userName ? { ...user, active: !user.active } : user,
-    );
-    resolve(updatedUsers);
-  });
+    ),
+  );
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const toggleUserState = (allUsers, userName) => {
+//   return new Promise(resolve => {
+//     const updatedUsers = allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user,
+//     );
+//     resolve(updatedUsers);
+//   });
+// };
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
